0ccbe500be23a3574f69fa036e45869f78cf988c
Gregory Zuckerman <gregory.zuckerman@gmail.com>
2020-09-23 22:53:00 +0100

    Update UpdateMTRBackground.ps1

--- UpdateMTRBackground.ps1
+++ UpdateMTRBackground.ps1
@@ -1,7 +1,7 @@
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://www.gregoryzuckerman.ie/CustomBackgroundTheme.png","C:\Users\Skype\AppData\Local\Packages\Microsoft.SkypeRoomSystem_8wekyb3d8bbwe\LocalState\>CustomBackgroundTheme.png")
+$WebClient.DownloadFile("https://github.com/gzuckerman/personal-website/raw/master/CustomBackgroundTheme.png","C:\Users\Skype\AppData\Local\Packages\Microsoft.SkypeRoomSystem_8wekyb3d8bbwe\LocalState\>CustomBackgroundTheme.png")
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://www.gregoryzuckerman.ie/SkypeSettings.xml","C:\Users\Skype\AppData\Local\Packages\Microsoft.SkypeRoomSystem_8wekyb3d8bbwe\LocalState\SkypeSettings.xml")
+$WebClient.DownloadFile("https://github.com/gzuckerman/personal-website/raw/master/SkypeSettings.xml","C:\Users\Skype\AppData\Local\Packages\Microsoft.SkypeRoomSystem_8wekyb3d8bbwe\LocalState\SkypeSettings.xml")
 if (-not (Test-Path -LiteralPath 'c:\temp')) {
         New-Item -Path 'c:\temp' -ItemType Directory 
         }
